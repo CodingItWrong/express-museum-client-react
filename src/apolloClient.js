bfe2a5e3a002c71c5f45344a888a3108746c0cbd
Josh Justice <josh.justice@accesscfa.com>
2019-08-31 18:11:36 -0400

    Set up subscription

--- src/apolloClient.js
+++ src/apolloClient.js
@@ -2,7 +2,33 @@ import { ApolloClient } from 'apollo-client';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import { HttpLink } from 'apollo-link-http';
 import { onError } from 'apollo-link-error';
-import { ApolloLink } from 'apollo-link';
+import { ApolloLink, split } from 'apollo-link';
+import { WebSocketLink } from 'apollo-link-ws';
+import { getMainDefinition } from 'apollo-utilities';
+
+const httpLink = new HttpLink({
+  uri: 'http://localhost:3000/graphql-sql'
+});
+
+const wsLink = new WebSocketLink({
+  uri: `ws://localhost:3000/graphql`,
+  options: {
+    reconnect: true
+  }
+});
+
+const link = split(
+  // split based on operation type
+  ({ query }) => {
+    const definition = getMainDefinition(query);
+    return (
+      definition.kind === 'OperationDefinition' &&
+      definition.operation === 'subscription'
+    );
+  },
+  wsLink,
+  httpLink,
+);
 
 const client = new ApolloClient({
   link: ApolloLink.from([
@@ -15,10 +41,7 @@ const client = new ApolloClient({
         );
       if (networkError) console.log(`[Network error]: ${networkError}`);
     }),
-    new HttpLink({
-      uri: 'http://localhost:3000/graphql-sql',
-      credentials: 'same-origin'
-    })
+    link
   ]),
   cache: new InMemoryCache()
 });
