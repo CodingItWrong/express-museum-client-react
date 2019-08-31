e20573893f58d2e9366a3e70b9c5a32bd424debb
Josh Justice <josh.justice@accesscfa.com>
2019-08-31 18:11:41 -0400

    Activate subscription

--- src/App.js
+++ src/App.js
@@ -1,15 +1,43 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useQuery } from '@apollo/react-hooks';
 import gql from 'graphql-tag';
 
-function App() {
-  const { loading, error, data } = useQuery(gql`
-    {
-      restaurants {
-        name
-      }
+const LOAD_RESTAURANTS_QUERY = gql`
+  {
+    restaurants {
+      name
     }
-  `);
+  }
+`
+
+const COMMENTS_SUBSCRIPTION_QUERY = gql`
+  subscription restaurantAdded {
+    restaurantAdded {
+      name
+    }
+  }
+`;
+
+const updateQuery = (previousResult, { subscriptionData }) => {
+  console.log({ previousResult, subscriptionData });
+  const newRestaurant = subscriptionData.data.restaurantAdded;
+  return {
+    restaurants: [...previousResult.restaurants, newRestaurant]
+  };
+};
+
+function App() {
+  const { loading, error, data, subscribeToMore } = useQuery(LOAD_RESTAURANTS_QUERY);
+
+  /* eslint-disable react-hooks/exhaustive-deps */
+  useEffect(() => {
+    console.log('calling subscribeToMore');
+    subscribeToMore({
+      document: COMMENTS_SUBSCRIPTION_QUERY,
+      updateQuery,
+    })
+  }, []);
+  /* eslint-enable  */
 
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error :(</p>;
